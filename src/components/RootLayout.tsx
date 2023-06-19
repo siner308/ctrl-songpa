7427035256aa224c162df3f56aaec1a2d67749b6
siner308 <siner308@gmail.com>
2023-06-20 01:40:34 +0900

    ga 추가

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -3,7 +3,7 @@ import NavBar from '@/components/NavBar';
 import Footer from '@/components/Footer';
 import Head from 'next/head';
 import useLocale from '@/hooks/useLocale';
-
+import Script from 'next/script';
 
 type Props = {
   children: React.ReactNode;
@@ -11,6 +11,7 @@ type Props = {
 
 export default function RootLayout({ children }: Props) {
   const locale = useLocale();
+  let dataLayer;
   return (
     <main
       style={{ height: '100vh' }}
@@ -18,10 +19,30 @@ export default function RootLayout({ children }: Props) {
     >
       <Head>
         <title>Ctrl Songpa</title>
-        <meta property="og:description" content="19 August 2023 (Sat) Songpa, Seoul, South Korea" />
-        <meta property="og:url" content={`https://ctrl-songpa.vercel.app/${locale}`} />
-        <meta property="og:title" content="Ctrl Songpa" />
-        <meta property='og:image' content='https://ctrl-songpa.vercel.app/irk.webp' />
+        <meta property="og:description" content="19 August 2023 (Sat) Songpa, Seoul, South Korea"/>
+        <meta property="og:url" content={`https://ctrl-songpa.vercel.app/${locale}`}/>
+        <meta property="og:title" content="Ctrl Songpa"/>
+        <meta property="og:image" content="https://ctrl-songpa.vercel.app/irk.webp"/>
+
+        <Script
+          strategy={'afterInteractive'}
+          src="https://www.googletagmanager.com/gtag/js?id=G-29EKQ0KSHJ"
+        />
+        <Script
+          id={'gtag-init'}
+          strategy={'afterInteractive'}
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag() {
+                dataLayer.push(arguments)
+            }
+            gtag('js', new Date());
+  
+            gtag('config', 'G-29EKQ0KSHJ');
+            `,
+          }}
+        />
       </Head>
       <NavBar/>
       <div className={'flex-1'}>
