7b78677fd74d1b2a67ea15b491fcbee1bd948ee2
siner308 <siner308@gmail.com>
2023-07-06 01:25:46 +0900

    activate rsvp

--- src/components/[locale]/RSVP.tsx
+++ src/components/[locale]/RSVP.tsx
@@ -4,14 +4,12 @@ import useTranslation from '@/hooks/useTranslation';
 
 export default function RSVP() {
   const i18n = useTranslation();
-  const onClick = () => {
-    alert(i18n.comingSoon);
-  };
 
   return (
     <div className={''}>
-      <div
-        onClick={onClick}
+      <Link
+        target={'_blank'}
+        href={'https://register.songpa.willbe.blue/'}
         className={'flex justify-center items-center ' +
           'mx-auto max-w-xs ' +
           'cursor-pointer ' +
@@ -19,9 +17,7 @@ export default function RSVP() {
           'h-10 text-center mt-8 bg-[#4ba5f5]'}
       >
         {i18n.rsvp}
-      </div>
-      {/*<Link target={'_blank'} className={''} href={'https://register.songpa.willbe.blue/'}>*/}
-      {/*</Link>*/}
+      </Link>
     </div>
   );
 }
